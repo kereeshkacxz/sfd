952bd90f6b05e82a3ddc45beb2fa0f850eb4dd2f
kereeshkacxz <kirillar2@yandex.ru>
2025-05-02 00:28:43 +0300

    asd

--- frontend/.next/types/app/layout.ts
+++ frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\twt56\Downloads\sfd\frontend\src\app\layout.tsx
+// File: /Users/kirillar2/Desktop/sfd/frontend/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
