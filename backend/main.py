863af7b805ca9f828b6476ee25577fdb44609cbe
kereeshkacxz <kirillar2@yandex.ru>
2025-05-12 22:49:04 +0300

    asd

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,31 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.database import engine, Base, check_and_create_tables
-from backend.routes import auth, tasks, notifications, statistics, machines, triggers, data_source, admins, workers
+from backend.routes import (
+    auth,
+    tasks,
+    notifications,
+    statistics,
+    machines,
+    triggers,
+    data_source,
+    admins,
+    workers,
+)
 from backend.utils.generator import start_generator
 
 app = FastAPI()
+
+# --- Разрешаем CORS ---
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],  # Разрешить ВСЕ домены. ИЛИ укажите ['http://localhost:3000'] для фронтенда.
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить любые методы (GET, POST и т.д.)
+    allow_headers=["*"],  # Разрешить любые заголовки
+)
 
 # Создание таблиц (если их нет)
 Base.metadata.create_all(bind=engine)
@@ -19,12 +41,15 @@ app.include_router(data_source)
 app.include_router(admins)
 app.include_router(workers)
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
 
 start_generator()
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
